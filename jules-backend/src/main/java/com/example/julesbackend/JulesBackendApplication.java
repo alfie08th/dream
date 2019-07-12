bb2990e6b22b6c9f0f90acf72413c47721f9eec1
rockefelerthe2nd <tsherab@nyc.yearup.org>
2019-07-12 01:35:27 -0400

    ui and backend added

---
+++ jules-backend/src/main/java/com/example/julesbackend/JulesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.julesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulesBackendApplication.class, args);
+	}
+
+}
---
+++ jules-backend/src/test/java/com/example/julesbackend/JulesBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.julesbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JulesBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
